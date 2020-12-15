562d6d1bc88cb9aa0c89bddd79ea41d9f6bc8a92
DronAce <okeynazone@gmail.com>
2020-12-15 10:23:40 -0500

    commit tree 8b9ae6623d9933b5b4752ba1d6893a8e4daff228

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,15 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+
+  end
+
+  def activate
+    set_student
+    @student.active = !@student.active 
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
@@ -13,4 +22,8 @@ class StudentsController < ApplicationController
     def set_student
       @student = Student.find(params[:id])
     end
+
+    def student_params
+      params.require(:student).permit(:active)
+    end 
 end
